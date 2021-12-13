c930e819b091ace93a39e7127d702dadf79cd8a1
Naveenkumar Mani <naveenkumar.mani@aspiresys.com>
2021-12-13 10:45:13 +0530

    acceskey updated

--- s3.py
+++ s3.py
@@ -1,17 +1,18 @@
 import boto3
 import os
+from acesskey import access_key_id,secret_access_key
 
-client = boto3.client('s3', aws_access_key_id= "AKIAZGSLVELN7ZZ726NJ",
- aws_secret_access_key = "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")
+client = boto3.client('s3', aws_access_key_id= access_key_id,
+ aws_secret_access_key = secret_access_key)
 
 s3 = boto3.resource('s3')
 print(s3.buckets.all())
 
-# print(os.getcwd())
-# print(os.listdir("D:\PythonExplore"))
-# for file in os.listdir():
-#     print(file)
-#     if '.py' in file:
-#         upload_file_bucket = 'firsttestbuct'
-#         upload_file_key = 'python/'+str(file)
-#         client.upload_file(file, upload_file_bucket, upload_file_key)
+print(os.getcwd())
+print(os.listdir("D:\PythonExplore"))
+for file in os.listdir():
+    print(file)
+    if '.py' in file:
+        upload_file_bucket = 'firsttestbuct'
+        upload_file_key = 'python/'+str(file)
+        client.upload_file(file, upload_file_bucket, upload_file_key)
