5de9b1c57868d8ffc455259496f76119d00f670f
Naveenkumar Mani <naveenkumar.mani@aspiresys.com>
2021-12-15 18:55:32 +0530

    Positional, Optional arguments

--- argparser.py
+++ argparser.py
@@ -1,6 +1,13 @@
 import argparse
 
-parser= argparse.ArgumentParser()
-parser.add_argument('--name', type=str, required=True)
+parser= argparse.ArgumentParser(description="Argparser Usage")
+
+#Positional arguments
+parser.add_argument('name',type=str, help='Name of the Programmer')
+
+
+#Optional arguments
+parser.add_argument('--mobile', type=str, required=True, help='Name of the mobile')
 args = parser.parse_args()
-print('Hello, ', args.name)
+
+print(args.name+"'s mobile brand is "+args.mobile)
