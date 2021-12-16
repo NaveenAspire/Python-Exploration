45f7b1db06667eefab21b0127b6a301870ed4067
Naveenkumar Mani <naveenkumar.mani@aspiresys.com>
2021-12-16 18:34:27 +0530

    Numpy Practice code

---
+++ numpyEx.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+print(np.__version__)
+
+lsarr = np.array([1,2,3,4])
+
+print(lsarr)
+
+print(type(lsarr))
+
+
+tparr = np.array((1,2,3,4,5))
+print("Accessing first element of 1-d array : ",tparr[0])
+
+print(tparr)
+print("Slicing on 1-d array : ",tparr[0:2])
+print("\n Dimension of above array",tparr.ndim,"\n")
+
+
+
+#Dimensions
+
+#2-D
+
+tdarr = np.array([[1,2,3],[4,5,6]])
+print(tdarr)
+print("Accessing first element of 2-d array : ",tdarr[0,0])
+print("Slicing on 2-d array : ",tdarr[1,0:2])
+print("Slicing on 2-d array : ",tdarr[0:2,2])
+print("\n Dimension of above array",tdarr.ndim,"\n")
+
+
+#3-D
+
+trdarr = np.array([[[1,2,3],[4,5,6]],[[1,2,3],[4,5,6]]])
+print(trdarr)
+print("Accessing first element of 3-d array : ",trdarr[0,0,0])
+print("\n Dimension of above array",trdarr.ndim,"\n")
+
+# n-D
+
+ndarr = np.array([1,2,3,4,5], ndmin=6)
+print(ndarr)
+print("\n Dimension of above array",ndarr.ndim,"\n")
