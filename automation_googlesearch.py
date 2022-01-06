4524c03f80ff4107b2a9f9c0c31b482c337d6368
Naveen <sknaveenkumarcse@gmail.com>
2022-01-06 06:24:23 -0500

    Updated

--- automation_googlesearch.py
+++ automation_googlesearch.py
@@ -1,14 +1,18 @@
-from selenium import webdriver
+"""This module code for send the serach things using argparser and
+ serach result will show in Google"""
 import argparse
+from selenium import webdriver
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--search', default='Aspire Systems')
+parser.add_argument("--search", default="Aspire Systems")
 args = parser.parse_args()
 
 driver = webdriver.Chrome()
-driver.get('https://www.Google.com/')
+driver.get("https://www.Google.com/")
 
-search  = driver.find_element_by_xpath('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div[2]/div[2]/input')
+search = driver.find_element_by_xpath(
+    "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div[2]/div[2]/input"
+)
 search.send_keys(args.search)
 
 search.submit()
